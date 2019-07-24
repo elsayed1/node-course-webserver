7e90f2b45d908d62904df61711ae895ea363ab7e
elsayed1 <elsayed_alaa70@yahoo.com>
2019-07-24 19:00:30 +0200

    update

--- app.js
+++ app.js
@@ -3,15 +3,14 @@ const express = require("express"),
   mongoose = require("mongoose"),
   bodyParser = require("body-parser"),
   cors = require("cors"),
-  passport = require("passport"),
-  path = require("path");
+  passport = require("passport");
 // Routes
 const users = require("./routes/api/users"),
   profile = require("./routes/api/profile"),
   posts = require("./routes/api/posts");
 // Default Settings
 const app = express(),
-  port = process.env.PORT || 5000,
+  port = process.env.PORT || 4000,
   //mongodbURI = 'mongodb+srv://seko:seko@mernstackfronttoback-qiptm.mongodb.net/test?retryWrites=true';
   mongodbURI = require("./config/keys").MONGO_URI;
 //mongodbURI = 'mongodb://localhost:27017/mernfronttoback'
@@ -32,15 +31,6 @@ app.use("/api/posts", posts);
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
   const path = require("path");
-  app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
-  });
-}
-
-// serve static assets if in production
-if (process.env.NODE_ENV === "production") {
-  // set static folder
-  app.use(express.static("client/build"));
   app.get("*", (req, res) => {
     res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
   });
