abb93af282a6e17cea8b1df34e45612d9b8953c3
elsayed1 <elsayed_alaa70@yahoo.com>
2019-07-24 18:50:46 +0200

    modify static files

--- app.js
+++ app.js
@@ -40,7 +40,7 @@ if (process.env.NODE_ENV === "production") {
 // serve static assets if in production
 if (process.env.NODE_ENV === "production") {
   // set static folder
-  express.static("client/build");
+  app.use(express.static("client/build"));
   app.get("*", (req, res) => {
     res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
   });
